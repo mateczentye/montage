4c253c40e6d4d3fc72218107e5382e7b68b48c0c
mateczentye <czentyemate@gmail.com>
2022-12-18 15:14:57 +0000

    changed release package name to stop conflict on pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='montage',
+    name='quick_montage',
     author='Máté I. Czentye',
     version='0.0.3',
     author_email='czentyemate@gmail.com',
