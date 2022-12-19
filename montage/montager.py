b4727ff20fe612be22ebe4a336f24ebbe8572ffb
mateczentye <czentyemate@gmail.com>
2022-12-19 23:30:45 +0000

    fixed import

--- montage/montager.py
+++ montage/montager.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 import numpy as np
 import os
-from utilities import loop_check
+from .utilities import loop_check
 
 
 def montager(name_array, dir):
